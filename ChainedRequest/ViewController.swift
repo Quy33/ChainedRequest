2e352a5c80586a623ee3e0d663ac84b2d5f262f4
Quy33 <87293639+Quy33@users.noreply.github.com>
2021-07-19 11:47:33 +0700

    GitHub URL
    Get URL

--- ChainedRequest/ViewController.swift
+++ ChainedRequest/ViewController.swift
@@ -31,3 +31,35 @@ struct Branch: Decodable {
     let name : String
     
 }
+
+class GitHubRepository{
+    private let netWorkService = NetWorkService()
+    private let baseURLString = "https://api.github.com"
+    
+    func getRepos() -> Observable<[Repo]>{
+        return netWorkService.execute(url: URL(string: baseURLString + "/repositories")! )
+    
+    }
+    
+    func getBranches(ownerName: String, repoName: String) -> Observable<[Branch]>{
+        return netWorkService.execute(url: URL(string: baseURLString + "/repos/\(ownerName)/\(repoName)/branches")! )
+    }
+}
+
+class NetWorkService {
+    func execute<T: Decodable>(url: URL) -> Observable<T>{
+        return Observable.create{ observer -> Disposable in
+            let task = URLSession.shared.dataTask(with: url){ data, _, _ in
+                
+                guard let data = data, let decoded = try? JSONDecoder().decode(T.self, from: data) else {return}
+            observer.onNext(decoded)
+                observer.onCompleted()
+            }
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+}
